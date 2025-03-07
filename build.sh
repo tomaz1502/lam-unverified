a2b326cab15e4e030c853e379754266876ef9638
Tomaz <tomgm1502@gmail.com>
2025-03-07 15:18:55 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-agda2hs ./src/Lam/Agda/Lam/Export.agda
-mv ./src/Lam/Agda/Lam/*.hs ./src/Lam
-rm ./src/Lam/Export.hs
+# agda2hs ./src/Lam/Agda/Lam/Export.agda
+# mv ./src/Lam/Agda/Lam/*.hs ./src/Lam
+# rm ./src/Lam/Export.hs
 stack build
